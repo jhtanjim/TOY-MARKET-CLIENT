8480f3b32bc4d0e2c0f91b51582a81861d3d16c0
jhtanjim~ <jh.tanjim.4@gmail.com>
2023-06-19 15:34:41 +0600

    done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,60 +1,53 @@
+
 import React from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
 
 const Banner = () => {
+
+
+
+
     return (
-        <div>
-            <div className="carousel w-full h-[800px]">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/sF4h6Sf/pexels-pixabay-163546.jpg" className="w-full" alt="Slide 1" />
+        <div className="carousel w-full h-">
+            <AwesomeSlider animation="cubeAnimation" className="slider">
+                <div className="bg-black">
+                    <img
+                        className=""
+                        src="https://i.ibb.co/5GLt3VQ/pexels-mike-bird-97353.jpg"
+                        alt="Slider 1"
+                    />
 
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle mr-5">
-                            ❮
-                        </a>
-                        <a href="#slide2" className="btn btn-circle">
-                            ❯
-                        </a>
-                    </div>
                 </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <img src="https://cdn.pixabay.com/photo/2016/11/06/05/33/miniature-1802333_960_720.jpg" className="w-full" alt="Slide 2" />
+                <div >
+                    <img
+                        className=""
+                        src="https://i.ibb.co/GnyjmhM/pexels-lorena-mart-nez-2320208.jpg"
+                        alt="Slider 2"
+                    />
 
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle mr-5">
-                            ❮
-                        </a>
-                        <a href="#slide3" className="btn btn-circle">
-                            ❯
-                        </a>
-                    </div>
                 </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img src="https://cdn.pixabay.com/photo/2014/11/23/12/11/toy-542701_960_720.jpg" className="w-full" alt="Slide 3" />
+                <div >
+                    <img
+                        className=""
+                        src="https://i.ibb.co/my5Z5pk/pexels-mike-bird-132539.jpg"
+                        alt="Slider 3"
+                    />
 
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle mr-5">
-                            ❮
-                        </a>
-                        <a href="#slide4" className="btn btn-circle">
-                            ❯
-                        </a>
-                    </div>
                 </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                    <img src="https://cdn.pixabay.com/photo/2017/02/22/18/52/model-car-2090291_960_720.jpg" className="w-full" alt="Slide 4" />
+                <div >
+                    <img
+                        className=""
+                        src="https://i.ibb.co/syJr7d4/pexels-revac-film-s-photography-552139.jpg"
+                        alt="Slider 4"
+                    />
 
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle mr-5">
-                            ❮
-                        </a>
-                        <a href="#slide1" className="btn btn-circle">
-                            ❯
-                        </a>
-                    </div>
                 </div>
-            </div>
+            </AwesomeSlider>
         </div>
     );
 };
 
 export default Banner;
+
